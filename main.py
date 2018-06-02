4a5d2f8ab4b069718ba2a751f207d8df6cb6488d
Major Péter <majorpeter29@gmail.com>
2018-06-02 21:43:30 +0200

    added simple js to reload stat page

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
     data.append(('Temperature', slave.get_temperature()))
     data.append(('RH', slave.get_humidity_percentage()))
     data.append(('Light', slave.get_light_lux()))
-    return str(data)
+    return '<html><head><script type="text/javascript">setTimeout(function(){location.reload();}, 1000);</script></head><body>' + str(data) + '</body></html>'
 
 
 if __name__ == '__main__':
