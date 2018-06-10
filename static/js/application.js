f0bbaf6b51f9575f5c9b74f129f9682558e2282b
Major Péter <majorpeter29@gmail.com>
2018-06-10 22:52:03 +0200

    hide on-off buttons when useless

--- static/js/application.js
+++ static/js/application.js
@@ -16,16 +16,29 @@ $(document).ready(function() {
     refresh_ledcolor();
 });
 
+function get_led_sliders_rgb() {
+    return $('input#led-red').val() + ',' + $('input#led-green').val() + ',' + $('input#led-blue').val();
+}
+
 function refresh_ledcolor() {
     $.get('/led', function(data) {
         rgb = data['rgb'].split(',');
         $('input#led-red').val(rgb[0]);
         $('input#led-green').val(rgb[1]);
         $('input#led-blue').val(rgb[2]);
+        refresh_ledbuttons();
     });
 }
 
 function update_ledcolor() {
-    rgb = $('input#led-red').val() + ',' + $('input#led-green').val() + ',' + $('input#led-blue').val();
-    $.post('/led', {rgb: rgb});
+    $.post('/led', {rgb: get_led_sliders_rgb()});
+    refresh_ledbuttons();
+}
+
+function refresh_ledbuttons() {
+    rgb = get_led_sliders_rgb();
+    on_display = (rgb != '255,255,255');
+    off_display = (rgb != '0,0,0');
+    $('a#led-on').css('display', on_display ? 'inline' : 'none');
+    $('a#led-off').css('display', off_display ? 'inline' : 'none');
 }
