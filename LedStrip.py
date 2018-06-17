e45dfdbd06461fc2816d106ee6de2d16f395fae4
Major Péter <majorpeter29@gmail.com>
2018-06-17 22:42:29 +0200

    created background thread in LedStrip to fix communication errors on frequent updates

--- LedStrip.py
+++ LedStrip.py
@@ -1,24 +1,36 @@
 import colorsys
+from threading import Thread, Event
+from time import sleep
 
 
 class LedStrip:
     def __init__(self, node):
         self.node = node
         self.rgb_colors = [0, 0, 0]
+        self.update_event = Event()
+        self.thread = Thread(target=self.thread_function)
+        self.thread.start()
 
     def set_color_rgb(self, rgb_colors):
         self.rgb_colors = rgb_colors
-        self.node.ColorRgb = LedStrip.color_array_to_str(self.rgb_colors)
+        self.update_event.set()
 
     def set_color_hsl(self, hsl_colors):
         self.rgb_colors = LedStrip.convert_hsl_to_rgb(hsl_colors)
-        self.node.ColorRgb = LedStrip.color_array_to_str(self.rgb_colors)
+        self.update_event.set()
 
     def get_color_rgb_str(self):
         return LedStrip.color_array_to_str(self.rgb_colors)
 
     def get_color_hsl_str(self):
         return LedStrip.color_array_to_str(LedStrip.convert_rgb_to_hsl(self.rgb_colors))
+
+    def thread_function(self):
+        while True:
+            self.update_event.wait()
+            self.update_event.clear()
+            self.node.ColorRgb = LedStrip.color_array_to_str(self.rgb_colors)
+            sleep(0.010)
 
     @staticmethod
     def color_array_to_str(array):
