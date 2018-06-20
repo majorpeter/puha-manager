626c1c8670a6235b76ef02ed96de3ddd8fabac65
Major Péter <majorpeter29@gmail.com>
2018-06-20 21:42:53 +0200

    implemented knight rider animation

--- Animation.py
+++ Animation.py
@@ -34,3 +34,39 @@ class LinearInterpolateAnimation(Animation):
             return [color] * self.led_count
         else:
             return None
+
+
+class KnightRiderAnimation(Animation):
+    def __init__(self, led_count, active_led_count, color, length, speed):
+        """
+        :param color: color of moving line
+        :param length: length of moving line (LED count)
+        :param speed: speed of movement in [LED/s]
+        """
+        super(KnightRiderAnimation, self).__init__(led_count)
+        self.active_led_count = active_led_count
+        self.color = color
+        self.length = length
+        self.speed = speed
+
+        self.position = 0
+        self.direction = 1
+        self.last_movement_time = datetime.now()
+
+    def step(self):
+        now = datetime.now()
+        delta_time = now - self.last_movement_time
+        delta_led = int(self.speed * (delta_time / timedelta(seconds=1)))
+        if delta_led > 0:
+            self.last_movement_time = now
+            self.position += self.direction * delta_led
+            if self.position >= self.active_led_count - self.length:
+                self.position = self.active_led_count - self.length
+                self.direction = -1
+            elif self.position <= 0:
+                self.position = 0
+                self.direction = 1
+        result = [[0, 0, 0]] * self.led_count
+        for i in range(self.position, self.position + self.length):
+            result[i] = self.color[:]
+        return result
