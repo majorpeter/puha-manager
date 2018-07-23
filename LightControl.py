925eb9e8e9a5f69a022a0c0f5c4a4ef196c7340d
Major Péter <majorpeter29@gmail.com>
2018-07-23 22:26:59 +0200

    LightControl is actually a control system with integrator anti-windup

--- LightControl.py
+++ LightControl.py
@@ -29,6 +29,15 @@ class LightControl:
         self.mode = mode
 
     def on_light_measurement_changed(self, measurement):
+        """
+        updates light control according to configuration
+        :param measurement: light measurement value (in lx)
+        Integrator anti-windup solution based on http://cse.lab.imtlucca.it/~bemporad/teaching/ac/pdf/AC2-09-AntiWindup.pdf
+          e -> error
+          K_p -> 'light_control_k_p'
+          K_p/T_i -> 'light_control_k_i'
+          1/T_t -> 'light_control_anti_windup_time'
+        """
         if self.mode == LightControl.Mode.Manual:
             return
 
@@ -38,18 +47,26 @@ class LightControl:
             target_illuminance = self.config['target_nighttime_illuminance']
 
         error = target_illuminance - measurement
+        # since the measurement is quantized, the error can never reach zero
         if abs(error) < self.config['light_sensor_quantization_error']:
-            return
+            error = 0
+
+        # update integrator
         self.integrator += error * self.config['light_control_k_i']
 
-        # limit range of integrator into the range of lightness (0-100)
-        if self.integrator < 0:
-            self.integrator = 0
-        elif self.integrator > 100:
-            self.integrator = 100
+        # output value (v)
+        lightness = self.config['light_control_k_p'] * error + self.integrator
+
+        # saturate output signal (u) into the range of lightness (0-100); handle integrator wind-up
+        if lightness < 0:
+            self.integrator += self.config['light_control_anti_windup_time'] * (0 - lightness)
+            lightness = 0
+        elif lightness > 100:
+            self.integrator += self.config['light_control_anti_windup_time'] * (100 - lightness)
+            lightness = 100
 
         self.led_strip.set_color_hsl([
             self.hue,
             self.saturation,
-            self.integrator
+            lightness
         ])
