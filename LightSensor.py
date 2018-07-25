769ed60693eed8031170a914e907c0aee7006227
Major Péter <majorpeter29@gmail.com>
2018-07-25 21:35:36 +0200

    using CHG's in LightSensor instead of periodic polling

--- LightSensor.py
+++ LightSensor.py
@@ -1,15 +1,10 @@
-from threading import Thread
-from time import sleep
-
-
 class LightSensor:
     def __init__(self, node):
         self.node = node
         self.illuminance = 0
         self.listeners = []
 
-        self.thread = Thread(target=self.thread_function)
-        self.thread.start()
+        self.node.Illuminance.subscribe_to_changes(self.on_property_changed)
 
     def add_listener(self, listener):
         """
@@ -18,12 +13,7 @@ class LightSensor:
         """
         self.listeners.append(listener)
 
-    def thread_function(self):
-        while True:
-            try:
-                self.illuminance = float(str(self.node.Illuminance))
-                for listener in self.listeners:
-                    listener(self.illuminance)
-            except BaseException:
-                print('Error while reading light sensor')
-            sleep(0.1)
+    def on_property_changed(self, name, value):
+        self.illuminance = float(value)
+        for listener in self.listeners:
+            listener(self.illuminance)
