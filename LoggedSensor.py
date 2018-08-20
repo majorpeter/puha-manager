6f3058101e369f8a86adec51680c70de5cdb64b4
Major Péter <majorpeter29@gmail.com>
2018-08-20 17:36:22 +0200

    refactor: use timestampts internally instead of datetime objects

--- LoggedSensor.py
+++ LoggedSensor.py
@@ -21,21 +21,22 @@ class LoggedSensor:
         self.holdoff_time = holdoff_time
         self.lock = Lock()
 
+
     def add_measurement(self, measurement):
-        now = datetime.now()
+        now = datetime.now().timestamp()
 
         with self.lock:
             if self.holdoff_time is not None:
                 if len(self.data) > 0:
                     diff = now - self.data[-1]['time']
-                    if diff < self.holdoff_time:
+                    if diff < self.holdoff_time.total_seconds():
                         return
 
             if len(self.data) > self.max_measurements:
                 del self.data[0]
 
             self.data.append({'time': now, 'measurement': measurement})
-            Database.instance.insert_measurement(now.timestamp(), self.type_id, measurement)
+            Database.instance.insert_measurement(now, self.type_id, measurement)
 
     def get_chart_data(self, from_timestamp=0):
         label = []
@@ -44,11 +45,12 @@ class LoggedSensor:
 
         with self.lock:
             for item in self.data:
-                if item['time'].timestamp() > from_timestamp:
-                    label.append(item['time'].strftime('%H:%M:%S'))
+                if item['time'] > from_timestamp:
+                    timestamp = datetime.fromtimestamp(item['time'])
+                    label.append(timestamp.strftime('%H:%M:%S'))
                     data.append('%.2f' % item['measurement'])
 
             if len(self.data) > 0:
-                last_timestamp = int(self.data[-1]['time'].timestamp())
+                last_timestamp = int(self.data[-1]['time'])
 
         return label, data, last_timestamp
